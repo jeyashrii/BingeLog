04e4ce7f2f0dc8d4ce12a31d77358f1bff05bf75
Jeyashri J <jeyashri30@gmail.com>
2024-10-25 23:04:43 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://jeyashri30:wDjatxWgeGjlZzZO@cluster0.919bis6.mongodb.net/moviesList?retryWrites=true&w=majority&appName=Cluster0";
+  "";
